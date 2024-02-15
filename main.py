b060011622848481c8c500e8853767c9425ff194
Foxtrot <sood.sarthak.99@gmail.com>
2024-02-16 01:46:56 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()  # Timmy object declared from Turtle class
+print(timmy)
+timmy.shape("turtle")  # object method / function
+timmy.color("coral")   # object method / function
+timmy.forward(100)     # object method / function
+
+
+my_screen = Screen()  # my_screen object declared from Screen class
+print(my_screen.canvheight)  # attribute declared from the object
+my_screen.exitonclick()
+#create a comment for github
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()  # Object table declared from Class PrettyTable
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])  # Object Method / function
+table.add_column("type", ["Electric", "Water", "Fire"])  # Object Method / function
+print(table)
+print(table.align)
+
+table.align = 'l'  # changing the value of object attribute / variable
+print(table)
+print(table.align)
